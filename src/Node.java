62437f3afb7c0a54cd71b0c331a29889ab6297a1
Norraniti-Yotha <31031648+Norraniti-Yotha@users.noreply.github.com>
2018-01-21 22:11:01 +0700

    Add files via upload
    Node & Tree

---
+++ src/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+/**
+ *
+ * @author User
+ */
+public class Node {
+    String key = ".";
+    Node Left = null;
+    Node Right = null;
+    
+            public Node(){
+            
+        }
+        public Node(String key){
+            this.key = key;
+            }
+
+}
