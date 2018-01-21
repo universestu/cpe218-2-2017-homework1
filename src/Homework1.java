8d6e8f8d825de8667b9b814edee7b0d5345312ea
Phumiphat_C <guitar11161@gmail.com>
2018-01-21 23:33:45 +0700

    test

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,15 +2,68 @@
 public class Homework1 {
 
 	public static void main(String[] args) {
+		String input = "";
 		// Begin of arguments input sample
 		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
+		    input = args[0];
+	}else System.exit(1);
 		// End of arguments input sample
 		
 		// TODO: Implement your project here
+	Node n = null;
+		for(int i=0; i<input.length(); i++){
+			Node tmp = new Node();
+			tmp.setValue(String.valueOf(input.charAt(i)));
+			tmp.setLeft(n);
+			n = infix(tmp);
+	}
+	String infix = inorder(n);
+		System.out.println(infix.substring(1,infix.length()-1) + "=" + calculator(n));
+}
+
+	public static Node infix(Node n){
+		if (!n.getValue().matches("[0-9]")) {
+			n.setRight(n.getLeft());
+			if (n.getLeft().getValue().matches("[0-9]")) {
+				n.setLeft(n.getRight().getLeft());
+				n.getRight().setLeft(null);
+			} else {
+				n.setRight(n.getLeft());
+				n.setLeft(n.getRight().getLeft().getLeft());
+				n.getRight().getLeft().setLeft(null);
+			}
+		}
+		return n;
+	}
+
+	public static String inorder(Node n) {
+		String left = "";
+		String right = "";
+		if (n.getLeft() != null) {
+			left = "(" + inorder(n.getLeft());
+		}
+		if (n.getRight() != null) {
+			right = inorder(n.getRight()) + ")";
+		}
+
+		return (left + n.getValue() + right);
+	}
+
+	public static int calculator(Node n) {
+
+		if (n.getValue().matches("[0-9]")) return Integer.valueOf(n.getValue());
+
+		int result = 0;
+		int left = calculator(n.getLeft());
+		int right = calculator(n.getRight());
+
+		switch (n.getValue()){
+			case "+":result = left + right;break;
+			case "-":result = left - right;break;
+			case "*":result = left * right;break;
+			case "/":result = left / right;break;
+		}
+
+		return result;
 	}
 }
---
+++ src/Node.java
@@ -0,0 +1,34 @@
+public class Node {
+    private Node left;
+    private Node right;
+    private String value;
+
+    Node(){
+        left = null;
+        right = null;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
