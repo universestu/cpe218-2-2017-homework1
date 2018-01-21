544add794013060bca610b5ba8d4bbe51a98738d
wirac <wirac@DESKTOP-CM32G4K>
2018-01-21 16:55:31 +0700

    590610660

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,109 @@
+//import java.util.EmptyStackException;
+import java.util.Scanner;
+import java.util.Stack;
+//import java.util.*;
+//import java.io.*;
+import org.w3c.dom.Node;
+//import org.w3c.dom.NodeList;
+import javax.imageio.metadata.IIOMetadataNode;
 
 public class Homework1 {
+	private static Scanner scanner;
+	static Stack restack = new Stack();
+	static Stack stack = new Stack();
+	static Node root;
+	public static Node current=root;
+	
+	
+	
+	private static boolean isOperator(String c){
+		  if(c == "+" || c == "-" || c == "*" || c =="/")
+		   return true;
+		  return false;
+		 }
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
+	private static boolean isOperator(char c){
+		  if(c == '+' || c == '-' || c == '*' || c =='/' || c == '^')
+		   return true;
+		  return false;
+		 }
+	
+	public static String infix(String postfix){
+		  Stack<String> s = new Stack<>();
+		   
+		  for(int i = 0; i < postfix.length(); i++){
+		   char c = postfix.charAt(i);
+		   if(isOperator(c)){
+		    String b = s.pop();
+		    String a = s.pop();
+		    s.push("("+a+c+b+")");
+		   }
+		   else
+		    s.push(""+c);
+		  }
+		  return s.pop();
+		 }
+
+	public static void inorder(Node n) {
+		char key = n.getNodeName().charAt(0);
+		if(!isOperator(key)){
+			n.setNodeValue(n.getNodeName());
+			return;
 		}
-		// End of arguments input sample
+		String secondData = stack.pop().toString();
+		Node secondNum = new IIOMetadataNode(secondData.toString());
+		inorder(secondNum);//re
+		String firstData = stack.pop().toString();
+		Node firstNum = new IIOMetadataNode(firstData.toString());
+		inorder(firstNum);//re
+		n.appendChild(secondNum);
+		n.appendChild(firstNum);
+		calculate(n);		
+}
+
+	
+	public static void calculate (Node n) {
+		String key = n.getNodeName(); 
+		String seNum = n.getChildNodes().item(0).getNodeValue();
+		String fiNum = n.getChildNodes().item(1).getNodeValue(); 
+		String result;
+		int num1 = Integer.valueOf(seNum);
+		int num2 = Integer.valueOf(fiNum);
+		switch (key) {
+    	case ("+"):	result = Integer.toString(num2+num1);
+    				n.setNodeValue(result);
+    				break;
+		case ("-"):	result = Integer.toString(num2-num1);
+					n.setNodeValue(result);
+					break;
+    	case ("*"):	result = Integer.toString(num2*num1);
+					n.setNodeValue(result);
+					break;
+    	case ("/"):	result = Integer.toString(num2/num1);
+					n.setNodeValue(result);
+					break;
+    	}	
 		
-		// TODO: Implement your project here
+		
+	}
+	
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		String data = scanner.nextLine();
+		char[] newData = data.toCharArray();
+		for(int i=0 ; i<data.length() ; i++) {
+			String x = Character.toString(newData[i]);
+			stack.push(x);
+			}
+		//System.out.println(stack);
+		System.out.print(infix(data) +"=");
+		root = new IIOMetadataNode(stack.pop().toString());
+		inorder(root);
+		System.out.println(root.getNodeValue());
 	}
 }
+		
+
+
+
+
