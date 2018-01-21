f098c0de2f021a2ef3f9c9755a2e891f462209ad
FaberCastell <vorason.si@gmail.com>
2018-01-21 16:41:50 +0700

    finish_1_hw1

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,68 @@
+import java.util.Stack;
 
 public class Homework1 {
+     static Stack s = new Stack();
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
+	public static void main (String[] args)  {
+        Node root = null;
 		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
+            String input = args[0];
+
+            for (int i = 0; i < input.length(); i++) {
+                String value = input.substring(i,i+1);
+                Node n = new Node(value,null,null);
+                infix(n);
+
+            }
+            root = (Node)s.pop();
+            String out = inorder(root);
+            System.out.println(out.substring(1,out.length()-1) + "=" + calculate(root) );
 		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
+
+    }
+
+
+	public static void infix(Node n){
+            if (n.node.matches("[0-9]")) {
+                s.add(n);
+
+            } else {
+                n.right = (Node) s.pop();
+                n.left = (Node) s.pop();
+                s.add(n);
+            }
+	}
+
+	public static String inorder(Node n){
+        String temp="";
+        if(n.left != null){
+            temp+= "(" + inorder(n.left);
+        }
+        temp+=n.node;
+        if(n.right != null){
+            temp+=inorder(n.right) + ")";
+        }
+        return temp ;
+    }
+
+	public static int calculate(Node n){
+
+        if (n.node.matches("[0-9]")){
+            return Integer.parseInt(n.node);
+        }
+        int value = 0;
+        int left = calculate(n.left);
+        int right = calculate(n.right);
+
+        if(n.node.equals("+")){
+            value = left+right;
+        }else if (n.node.equals("-")){
+            value = left-right;
+        }else if (n.node.equals("*")){
+            value = left*right;
+        }else if (n.node.equals("/")){
+            value = left/right;
+        }
+        return value;
 	}
 }
---
+++ src/Node.java
@@ -0,0 +1,18 @@
+/**
+ * Created by FaberCastell on 1/21/2018.
+ */
+public class Node {
+    public Node left;
+    public Node right;
+    public String node;
+
+    Node(String x,Node l,Node r){
+        node = x;
+        left = l;
+        right = r;
+
+
+    }
+
+
+}
