5daa0be57b7e9447c0064aeffd0dca43ccc71e50
poppybsr <poppy.bt120@gmail.com>
2018-01-21 23:19:51 +0700

    Update Homework1.java
    ส่งไปก่อนละกัน

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,6 +1,12 @@
+package homework1;
+import java.util.*;
+import java.util.Stack;
+import java.util.Scanner;
+import javafx.scene.Node;
+import javax.swing.JTree;
 
-public class Homework1 {
-
+public class Homework1{
+        public static Stack<Character> stack = new Stack<Character>();
 	public static void main(String[] args) {
 		// Begin of arguments input sample
 		if (args.length > 0) {
@@ -13,4 +19,64 @@ public class Homework1 {
 		
 		// TODO: Implement your project here
 	}
+       public static class Node {
+            char value;
+            Node left,right;
+    
+            Node (char item){
+                value = item;
+                left = right = null;
+        
+    }
 }
+       public static void inflix(Node n){
+           if(n.value=='+'||n.value=='-'||n.value=='*'||n.value=='/'){
+               n.right =new Node(stack.pop());
+               inflix(n.right);
+               n.left =new Node(stack.pop());
+               inflix(n.left);
+               
+           }
+	
+       }
+        public boolean isOperator(char op){
+                switch(op){
+                        case '+':
+                                return true;
+                        case '-':
+                                return true;
+                        case '/':
+                                return true;
+                        case '*':
+                                return true;
+                        case '^':
+                                return true;
+                        default:
+                                return false;
+                }
+        }
+        public static void inorder(Node n) {
+            
+                    if (n != null) {
+                            inorder(n.left);
+                            System.out.print(n.value + " ");
+                            inorder(n.right);
+                    }
+        }
+        
+        public static int calculator(Node n){
+            switch(n.value){
+                case'+':
+                    return calculator(n.left)+calculator(n.right);
+                case'-':
+                    return calculator(n.left)-calculator(n.right);
+                case'*':
+                    return calculator(n.left)*calculator(n.right);
+                case'/':
+                    return calculator(n.left)/calculator(n.right);
+                    
+            }
+            return 0;
+        
+        }
+    }
