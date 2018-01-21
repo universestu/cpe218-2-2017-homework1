a07c7bc6290a4f1378bc70ef9bdfdfc88799d5ad
somprasertTS <ssom.ts@gmail.com>
2018-01-21 16:27:08 +0700

    test

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,42 @@
 
+import java.util.Stack;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import package.Node;
+
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
+	static String infix="" ;
+ 	public static void main(String[] args) throws ScriptException {
+		Node root = null;
+		for (int i = 0; i < args.length; i++) {
+			Stack<Node> mem = new Stack();
+			for (int j = 0; j < args[i].length(); j++) {
+				mem.push(new Node(args[i].toCharArray()[j]));
+
 			}
 		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
+		root = CreateTree(args[i].toCharArray());
+	}
+
+	public static boolean IsOperator (char c){
+		if(c == '+' || c == '-' || c == '*' || c == '/'){
+			return true;
+		}
+		return false;
+	}
+
+	public static Node CreateTree(char []postfix){
+
 	}
 }
+
+	// End of arguments input sample
+		// TODO: Implement your project here
+
+
+
+
+
+
