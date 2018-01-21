70cfffb67437fcfe6117e08bfdff481794efa6e1
ขี้เอง <ping31345@gmail.com>
2018-01-21 23:48:04 +0700

    fin? ;-;

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    public Node left ;
+    public Node right ;
+    public String node;
+    Node(String no,Node le,Node ri)
+    {
+        node = no;
+        left = le;
+        right = ri;
+    }
+
+
+}
