3fe09149356c8ce6af0697d03b503a8c7ad5016e
krisornkachai <31123619+krisornkachai@users.noreply.github.com>
2018-01-21 13:19:04 +0700

    finish

---
+++ src/Node.java
@@ -0,0 +1,24 @@
+
+
+public class Node  {
+
+    Node left;
+    Node right;
+    boolean isRoot=false;
+    char data;
+
+    public Node(char args) { 
+     this.data=args;    
+    }
+    
+     public Node(){   
+    
+    }
+
+
+
+   
+
+    
+  
+}
