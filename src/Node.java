fa529fdd5821b4d31db5ecc6ab482637913ad6a4
magmanex <arinchai.nanjaruwong@gmail.com>
2018-01-21 22:42:14 +0700

    Remove function in node

--- src/Node.java
+++ src/Node.java
@@ -7,16 +7,4 @@ public class Node {
         left = null;
         right = null;
     }
-
-    public void SetLeft(Node left_node) {
-        this.left = left_node;
-    }
-
-    public void  SetRight(Node right_node) {
-        this.right = right_node;
-    }
-
-    public void SetValueNode(char Value) {
-        this.value_node = Value;
-    }
 }
