8e2a4cd1b012ed025afe8e0d67c1fc41bb7827a8
Promyot <Promyot@10.80.198.193>
2018-01-21 23:41:45 +0700

    580610659-1

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,101 @@
+import java.util.Stack;
+
 
 public class Homework1 {
-
+	
+	public static  Node tree ;
+	
+	public static Stack<Character> sk =new Stack<Character>();
+	
 	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
-		// End of arguments input sample
 		
-		// TODO: Implement your project here
+		String input ="251-*32*+";
+
+		for(int i=0;i<input.length();i++){
+			sk.add(input.charAt(i));
+		}
+		tree = new Node(sk.pop());
+		
+		infix(tree);
+		
+		inorder(tree);
+		
+		System.out.print("="+Calculate(tree));
+		
+
 	}
+	public static void infix(Node n){
+		
+		if(n.key == '+' || n.key == '-' || n.key == '*' || n.key == '/'){
+			
+			n.right = new Node(sk.pop());
+			
+			infix(n.right);
+			
+			n.left = new Node(sk.pop());
+			
+			infix(n.left);
+			
+		}
+		
+	}
+	
+	public static void inorder(Node n) {
+		
+		if(n.key == '+' || n.key == '-' || n.key == '*' || n.key == '/')
+		{
+			if(n!=tree)System.out.print("(");
+			
+			inorder(n.left);
+			
+			System.out.print(n.key);
+			
+			inorder(n.right);
+			
+			if(n!=tree)System.out.print(")");
+		}else
+			
+		{
+			System.out.print(n.key);
+			
+		}
+	}
+	public static int Calculate(Node n){
+		
+		if(n.key == '+')
+			
+		{
+			return Calculate(n.left)+Calculate(n.right);
+			
+		}
+		
+		else  if(n.key == '-')
+			
+		{
+			return Calculate(n.left)-Calculate(n.right);
+			
+		}
+		else  if(n.key == '*')
+			
+		{
+			return Calculate(n.left)*Calculate(n.right);
+			
+		}
+		else  if(n.key == '/')
+			
+		{
+			
+			return  Calculate(n.left)/Calculate(n.right);
+			
+		}
+		
+		else return Integer.parseInt(n.key.toString());
+		
+	}
+	
+
+
+
+	
+	
 }
---
+++ src/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+
+    public Node right ;
+
+    public Node left ;
+
+    Character key ;
+
+    public Node (char input )
+    {
+        key = input;
+    }
+
+}
