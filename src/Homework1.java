8aff9ca48e1682a708042c225494df29af69ad13
supitch <sudukutoto@hotmail.com>
2018-01-21 21:39:17 +0700

    hw1

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,89 @@
+import java.util.Stack;
 
 public class Homework1 {
+	public static node Tree;
+	public static Stack <Character> stackold = new Stack<Character>();
+	public static Boolean IsNumber(Character character)
+	{
+		if("0123456789".indexOf(character.toString())!=-1)
+		{
+			return true;
+		}
+		return false;
+	}
 
+	public static Boolean Isoperand(Character character)
+	{
+		if("+-*/".indexOf(character.toString())!=-1)
+		{
+			return true;
+		}
+		return false;
+	}
 	public static void main(String[] args) {
-		// Begin of arguments input sample
+
+		String input="251-*32*+";
 		if (args.length > 0) {
-			String input = args[0];
+			input = args[0];
 			if (input.equalsIgnoreCase("251-*32*+")) {
 				System.out.println("(2*(5-1))+(3*2)=14");
 			}
 		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
+		for (int i = 0; i <input.length() ; i++) {
+			stackold.add(input.charAt(i));
+		}
+		Tree = new node(stackold.pop());
+		infix(Tree);
+		inorder(Tree);
+
+		System.out.println("="+calculate(Tree));
+	}
+
+
+
+	public  static void inorder(node n) {
+		if (Isoperand(n.k)) {
+			if (n != Tree)System.out.print("(");
+			inorder(n.left);
+			System.out.print(n.k);
+			inorder(n.right);
+			if(n != Tree) System.out.print(")");
+		}
+		else
+		{
+			System.out.print(n.k);
+		}
+	}
+
+	public static void  infix(node n) {
+		if (Isoperand(n.k)){
+			n.right = new node(stackold.pop());
+			infix(n.right);
+			n.left = new node(stackold.pop());
+			infix(n.left);
+		}
+
+	}
+	public static int calculate(node n){
+		if (Isoperand(n.k)){
+			if(n.k == '+'){
+				return calculate(n.left)+calculate(n.right);
+			}
+			else if (n.k == '-'){
+				return calculate(n.left)-calculate(n.right);
+			}
+			else if(n.k == '*'){
+				return calculate(n.left)*calculate(n.right);
+			}
+			else if(n.k == '/'){
+				return calculate(n.left)/calculate(n.right);
+			}
+		}
+		else if(IsNumber(n.k)){
+			return Integer.parseInt(n.k.toString()) ;
+		}
+
+		return 0;
+
 	}
 }
---
+++ src/node.java
@@ -0,0 +1,16 @@
+/**
+ * Created by supitch on 1/21/18.
+ */
+public class node {
+
+    public node left;
+    public node right;
+        Character k;
+
+        public node(char key)
+        {
+            k = key;
+        }
+
+}
+
