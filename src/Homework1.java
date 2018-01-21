ed2bd3eebf6ddaf5ffcd020298b811c9c459a3dd
Fluketaokung <fluketaokung@gmail.com>
2018-01-21 21:14:08 +0700

    Update Homework1.java
    homework

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,107 @@
 
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
-	}
+    static int i = -1;
+    static Tree tree = new Tree();
+    static String[] k;
+	
+
+    public static void main(String[] args) {
+        System.out.print("Input:");
+       
+        Scanner Input = new Scanner(System.in);
+       
+        String s = Input.nextLine();
+       
+        k = s.split("");
+       
+        maketree();
+        
+        inflix(tree.root);
+
+        System.out.println("=" + calculate(tree.root));
+
+
+
+    }
+
+    public static void maketree() {
+        i  =k.length-1;
+        tree.root = new Node(k[i]);i--;        
+            
+                tree.root.right = new Node();
+                tree.root.left = new Node();
+                
+           chack(tree.root.right);
+           chack(tree.root.left);
+            
+    }
+    
+    public void Inorder(Node a) {
+        if(a.left != null){
+            Inorder(a.left);
+        }
+        System.out.print(a.key);
+        if(a.right != null){
+            Inorder(a.right);
+        }
+    }
+   public static void chack (Node current) {
+       current.key = k[i];
+       if(Operator(k[i])){
+           current.right = new Node();
+           current.left = new Node();
+           i--;
+           chack(current.right);
+           chack(current.left);
+       }
+       else{
+           i--;
+       }
+  }
+
+    public static boolean Operator(String k) {
+           switch(k) {
+                case "+" : return true;
+                case "-" : return true;
+                case "*" : return true;
+                case "/" : return true;
+                default : return false;
+        }
+    }
+
+    public static int calculate(Node a) {
+        if (Operator(a.key)) {
+            switch(a.key) {
+                case "+" : return calculate(a.left) + calculate(a.right);
+                case "-" : return calculate(a.left) - calculate(a.right);
+                case "*" : return calculate(a.left) * calculate(a.right);
+                case "/" : return calculate(a.left) / calculate(a.right);
+                default :; return 0;
+        }
+        
+        }
+            else{
+            return Integer.parseInt(a.key);
+    }
+}
+       public static void inflix(Node a) {
+        if(Operator(a.key) && a != tree.root){
+            System.out.print("(");
+        }
+       
+        if(a.left != null){
+            inflix(a.left);
+        }
+       
+        System.out.print(a.key);
+       
+        if(a.right != null){
+            inflix(a.right);
+        }
+       
+        if(Operator(a.key) && a != tree.root){
+            System.out.print(")");
+        }
+    }
 }
