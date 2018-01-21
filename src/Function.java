23ed0774ff5c4e65093286b04d083bf124dfe287
drum2007 <beam555@hotmail.com>
2018-01-21 21:56:41 +0700

    first round HW1

---
+++ src/Function.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+public class Function {
+
+    public static Stack stack = new Stack();
+
+    public void infix(Node N) {
+        if (N.node.matches("[0-9]")) {
+            stack.push(N);
+        } else {
+            N.rightNode = (Node) stack.pop();
+            N.leftNode = (Node) stack.pop();
+            stack.push(N);
+        }
+    }
+
+    public String inorder(Node N) {
+        String temp = "";
+        if (N.leftNode != null) {
+            temp += "(" + inorder(N.leftNode);
+        }
+        temp += N.node;
+        if (N.rightNode != null) {
+            temp += inorder(N.rightNode) + ")";
+        }
+        return temp;
+    }
+
+    public int calculate(Node N) {
+
+        if (N.node.matches("[0-9]")) {
+            return Integer.parseInt(N.node);
+        }
+
+        int value = 0;
+        int left = calculate(N.leftNode);
+        int right = calculate(N.rightNode);
+
+        if (N.node.equals("+")) {
+            value = left + right;
+        } else if (N.node.equals("-")) {
+            value = left - right;
+        } else if (N.node.equals("*")) {
+            value = left * right;
+        } else if (N.node.equals("/")) {
+            value = left / right;
+        }
+
+        return value;
+    }
+}
--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,38 @@
-
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
-	}
+    public static Node root = null;
+    public static String output;
+
+    public static void main(String[] args) {
+
+        Function callFunction = new Function();
+
+        // Begin of arguments input sample
+        if (args.length > 0) {
+            String input = args[0];
+
+            for (int i = 0; i < input.length(); i++) {
+                String value = input.substring(i, i + 1);
+
+                Node N = new Node(value, null, null);
+
+                callFunction.infix(N);
+            }
+
+            root = (Node) callFunction.stack.pop();
+            output = callFunction.inorder(root);
+
+            System.out.println(output.substring(1,output.length()-1) + "=" + callFunction.calculate(root));
+
+//			if (input.equalsIgnoreCase("251-*32*+")) {
+//				System.out.println("(2*(5-1))+(3*2)=14");
+//			}
+        }
+        // End of arguments input sample
+
+        // TODO: Implement your project here
+    }
+
+
 }
+
