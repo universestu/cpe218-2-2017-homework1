1590051915e2469749927f74392003ed73ada9e3
Taopat <Oat22@windowslive.com>
2018-01-21 18:03:32 +0700

    edit newfile

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,101 @@
+import  java.util.Stack;
+
 
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
+	public static Node tree;
+	public static Stack <Character> KK = new Stack<Character>();
+	public static void inorder (Node node)
+	{
+		if (node.k =='+')
+		{
+			if(node!=tree) System.out.print("(");
+			inorder(node.left);
+			System.out.print("+");
+			inorder(node.right);
+			if(node!=tree)System.out.print(")");
+
 		}
+		else if (node.k =='-')
+		{
+			if(node!=tree) System.out.print("(");
+			inorder(node.left);
+			System.out.print("-");
+			inorder(node.right);
+			if(node!=tree)System.out.print(")");
+
+		}
+		else if (node.k =='*')
+		{
+			if(node!=tree) System.out.print("(");
+			inorder(node.left);
+			System.out.print("*");
+			inorder(node.right);
+			if(node!=tree)System.out.print(")");
+
+		}
+		else if (node.k =='/')
+		{
+			if(node!=tree) System.out.print("(");
+			inorder(node.left);
+			System.out.print("/");
+			inorder(node.right);
+			if(node!=tree)System.out.print(")");
+
+		}
+		else System.out.print(node.k);
+
+	}
+	public static void infix (Node inf )
+	{
+		if(inf.k =='+'||inf.k =='-'||inf.k =='*'||inf.k =='/')
+		{
+			inf.right=new Node(KK.pop());
+			infix(inf.right);
+			inf.left=new Node (KK.pop());
+			infix(inf.left);
+		}
+	}
+	public static int calculate (Node cal)
+	{
+		if(cal.k == '+')
+		{
+			return calculate(cal.left)+calculate(cal.right);
+		}
+		else if(cal.k == '-')
+		{
+			return calculate(cal.left)-calculate(cal.right);
+		}
+		else if(cal.k == '*')
+		{
+			return calculate(cal.left)*calculate(cal.right);
+		}
+		else if(cal.k == '/')
+		{
+			return calculate(cal.left)/calculate(cal.right);
+		}
+		else return  Integer.parseInt(cal.k.toString());
+	}
+
+
+
+	public static void main(String[] args)
+	{
+		// Begin of arguments input sample
+		String profix = args[0];
+		for(int i=0;i<profix.length();i++)
+		{
+			KK.add(profix.charAt(i));
+		}
+		tree = new Node (KK.pop());
+		infix(tree);
+		inorder(tree);
+		calculate(tree);
+		System.out.print("="+calculate(tree));
 		// End of arguments input sample
-		
+
 		// TODO: Implement your project here
 	}
 }
+
+
