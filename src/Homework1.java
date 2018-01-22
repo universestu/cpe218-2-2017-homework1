1a97c695eac5230e63aef62d8243d88223388da9
poppybsr <poppy.bt120@gmail.com>
2018-01-22 15:49:53 +0700

    Update Homework1.java
    รอบ 2

--- src/Homework1.java
+++ src/Homework1.java
@@ -6,41 +6,46 @@ import javafx.scene.Node;
 import javax.swing.JTree;
 
 public class Homework1{
-        public static Stack<Character> stack = new Stack<Character>();
-	public static void main(String[] args) {
+    public static Node tree;
+    public static Stack<Character> stack = new Stack<Character>();
+    public static void main(String[] args) {
 		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
+		String example = "251-*32*+";
+                 for(int i = 0; i < example.length(); i++)
+                 {
+                     stack.push(example.charAt(i));
+                     //System.out.print(EX);
+                 }
+                 tree = new Node(stack.pop());
+                 infix(tree);
+                 inorder(tree);
+                 System.out.print("="+calculator(tree));
 		// End of arguments input sample
 		
 		// TODO: Implement your project here
 	}
-       public static class Node {
-            char value;
+    public static class Node {
+            Character value;
             Node left,right;
     
             Node (char item){
                 value = item;
-                left = right = null;
         
     }
 }
-       public static void inflix(Node n){
+
+    public static void infix(Node n){
            if(n.value=='+'||n.value=='-'||n.value=='*'||n.value=='/'){
                n.right =new Node(stack.pop());
-               inflix(n.right);
+               infix(n.right);
                n.left =new Node(stack.pop());
-               inflix(n.left);
+               infix(n.left);
                
            }
 	
        }
-        public boolean isOperator(char op){
-                switch(op){
+    public static boolean isOperator(Node op){
+                switch(op.value){
                         case '+':
                                 return true;
                         case '-':
@@ -55,16 +60,35 @@ public class Homework1{
                                 return false;
                 }
         }
-        public static void inorder(Node n) {
-            
-                    if (n != null) {
-                            inorder(n.left);
-                            System.out.print(n.value + " ");
-                            inorder(n.right);
-                    }
+    public static void inorder(Node n) {
+            if(n!=null)
+        {   //System.out.print("(");
+            inorder(n.left);
+            System.out.print(n.value);
+            inorder(n.right);  
+            //System.out.print(")");
+        } 
         }
         
         public static int calculator(Node n){
+       /* if (n.left == null && n.right == null)
+            return 0;
+        else
+        {
+            int result = 0;
+            int left = calculator(n.left);
+            int right = calculator(n.right);
+ 
+            switch (n.value)
+            {
+            case '+' : result = left + right; break;
+            case '-' : result = left - right; break;
+            case '*' : result = left * right; break;
+            case '/' : result = left / right; break;
+            default : Integer.parseInt(n.value.toString());
+            }
+            return result;
+        }*/
             switch(n.value){
                 case'+':
                     return calculator(n.left)+calculator(n.right);
@@ -74,9 +98,16 @@ public class Homework1{
                     return calculator(n.left)*calculator(n.right);
                 case'/':
                     return calculator(n.left)/calculator(n.right);
+                default:
+                    return Integer.parseInt(n.value.toString());
                     
             }
-            return 0;
         
         }
     }
+
+
+    
+
+
+
