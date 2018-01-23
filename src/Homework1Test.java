284e8fa2d66fceb8d2419b637654a388360cf24b
Titipat <ttpsvb@gmail.com>
2018-01-23 11:35:37 +0700

    More test case

--- src/Homework1Test.java
+++ src/Homework1Test.java
@@ -13,6 +13,13 @@ public class Homework1Test {
 	@Test
 	public void testMain() {
 		theTestCaseList.add(new CalculatorTestCase("251-*32*+", "(2*(5-1))+(3*2)=14"));
+		theTestCaseList.add(new CalculatorTestCase("51+", "5+1=6"));
+		theTestCaseList.add(new CalculatorTestCase("51-", "5-1=4"));
+		theTestCaseList.add(new CalculatorTestCase("51*", "5*1=5"));
+		theTestCaseList.add(new CalculatorTestCase("51/", "5/1=5"));
+		theTestCaseList.add(new CalculatorTestCase("1251-*32*+*", "1*((2*(5-1))+(3*2))=14"));
+		theTestCaseList.add(new CalculatorTestCase("13-57*/9+", "((1-3)/(5*7))+9=9"));
+		
 		
 		for (CalculatorTestCase testCase: theTestCaseList) {
 		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -22,7 +29,7 @@ public class Homework1Test {
 		    Homework1.main(new String[] {testCase.getQuestion()});
 		    System.out.flush();
 		    System.setOut(old);
-		    assertEquals(baos.toString().replaceAll("\\s+",""), testCase.getAnswer());
+		    assertEquals(baos.toString().replaceAll("\n", ""), testCase.getAnswer());
 		}
 	}
 
